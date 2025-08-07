c582cb37cfde11ca59298ed119e975cca22fbaac
gschussler <grantschussler@gmail.com>
2025-08-07 15:34:16 -0500

    test log reformatting

--- server/goaccess/run-goaccess-loop.sh
+++ server/goaccess/run-goaccess-loop.sh
@@ -3,5 +3,5 @@ echo "[goaccess-loop] Starting GoAccess update loop (PID $$)..."
 while true; do
   echo "[goaccess-loop] Running update at $(date)"
   /usr/local/bin/goaccess-wrapper.sh
-  sleep 1800
+  sleep 10
 done
