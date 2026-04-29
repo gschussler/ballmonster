75df6eeeb00b71bb9e0b2274c7bb9c27327d68c1
gschussler <grantschussler@gmail.com>
2026-04-29 11:14:23 -0500

    added HTML to dev server coverage, updated copyright

--- esbuild.watch.js
+++ esbuild.watch.js
@@ -1,4 +1,6 @@
 import esbuild from 'esbuild';
+import chokidar from 'chokidar';
+import fsp from 'fs/promises';
 import path from 'path';
 
 const CONFIG = {
@@ -17,3 +19,22 @@ const ctx = await esbuild.context({
 
 await ctx.watch();
 console.log('esbuild is watching for JS changes...');
+
+const htmlFiles = [
+  // 'index.html.tmpl',
+  'index.html',
+  'pages/offense.html',
+  'pages/defense.html',
+  'pages/more.html',
+  '404.html'
+];
+
+chokidar.watch(htmlFiles.map(f => path.join(CONFIG.SRC_DIR, f))).on('change', async (filePath) => {
+  const relPath = path.relative(CONFIG.SRC_DIR, filePath);
+  const destPath = path.join(CONFIG.BUILD_DIR, relPath);
+  await fsp.mkdir(path.dirname(destPath), { recursive: true });
+  await fsp.copyFile(filePath, destPath);
+  // console.log(`Copied ${relPath} changes to dist`);
+});
+
+console.log('chokidar is watching for HTML changes...');
